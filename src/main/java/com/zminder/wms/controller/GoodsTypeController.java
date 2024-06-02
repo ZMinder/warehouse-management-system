c1a8fab5e59400940365d1c2cfca05f56a6877a5
CMinder <minderctrl@gmail.com>
2024-06-02 12:27:09 +0800

    1.修复了请求路径的问题

--- src/main/java/com/zminder/wms/controller/GoodsTypeController.java
+++ src/main/java/com/zminder/wms/controller/GoodsTypeController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/storage")
+@RequestMapping("/goodsType")
 @CrossOrigin
 public class GoodsTypeController {
     @Autowired
